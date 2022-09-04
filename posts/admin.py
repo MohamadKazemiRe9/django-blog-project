7af2c9220e6b7cf28b26f089dba0340bc840938e
Mohammad Kazemi Renany <mohammadkazemire9@gmail.com>
2022-09-05 03:14:44 +0430

    part 4

--- posts/admin.py
+++ posts/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Article
 # Register your models here.
 
-admin.site.register(Article)
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "status"]
+    list_filter = ("status",)
+    search_fields = ("title", "author__username")
