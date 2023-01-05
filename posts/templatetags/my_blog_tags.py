31e4e253dd436531de7e6c7a12b046cbfd153a0e
Mohammad Kazemi Renany <mohammadkazemire9@gmail.com>
2023-01-05 14:04:26 +0330

    part 11

--- posts/templatetags/my_blog_tags.py
+++ posts/templatetags/my_blog_tags.py
@@ -1,8 +1,17 @@
 from django import template
 from ..models import Article
+from datetime import datetime
 
 register = template.Library()
 
 @register.simple_tag
 def total_articles():
     return Article.publish.count()
+
+@register.filter(name="isnew")
+def time_calculate(value):
+    created_date = value.strftime("%d %m %y")
+    now_date = datetime.now().strftime("%d %m %y")
+    created_date_prime = datetime.strptime(created_date, "%d %m %y")
+    now_date_prime = datetime.strptime(now_date, "%d %m %y")
+    return (now_date_prime - created_date_prime).days < 7
